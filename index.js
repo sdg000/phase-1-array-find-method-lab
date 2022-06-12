c9f43cb9bd9942a31116a607ce144bc5b7b81803
Seth David Gyimah <esec26@gmail.com>
2022-06-12 17:25:24 +0000

    phase 1 array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find((object) => object.result === 'W')
+        if(result) {
+            const year = result.year
+            return year
+        }else {
+            return undefined
+        }
+}
